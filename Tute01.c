61c2667a44db0e49c28a878f2f79637bde1980d5
IT21020544 <87351016+IT21020544@users.noreply.github.com>
2021-07-13 03:56:29 +0000

    Code which inside of main function

--- Tute01.c
+++ Tute01.c
@@ -6,6 +6,19 @@
 
 int main() {
   
+  int no1, no2, total = 0;
+  float avg = 0;
+  
+  printf("Enter number 1:");
+  scanf("%d", &no1);
+
+  printf("Enter number 2:");
+  scanf("%d", &no2);
+  total = no1+no2;
+  avg = total/2.0;
+
+  printf("Average is %.2f", avg);
+  
   return 0;
 }
 
